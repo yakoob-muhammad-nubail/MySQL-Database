08d2026d4748a631ca2d92421a156065ac2f1154
yakoobmuhammad <yakooobnmuhammad.4@gmail.com>
2024-06-18 02:33:51 -0400

    initial commit, file relocation, flask and venv installed

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({'message':'Hello World'})
+
+if __name__ == 'main':
+    app.run(debug=True)
