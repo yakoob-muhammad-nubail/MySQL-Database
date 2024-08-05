3d2f39c45efb4419704ec7fdc0c3ec0b537012da
yakoobmuhammad <yakooobnmuhammad.4@gmail.com>
2024-08-05 01:56:28 -0400

    sign up page frontend setup

--- website/static/js/sign_up.js
+++ website/static/js/sign_up.js
@@ -189,3 +189,47 @@ mainNavbarHeaderNavAccountLogout.addEventListener("mouseout", function hoverOutL
 });
 
 mainNavbarHeaderNavAccountText.addEventListener("mousedown", openCloseAccountMenu);
+
+//form
+var loginForm = document.getElementById("signup_form");
+var loginFormEmailTitle = document.getElementById("signup_form_email_title");
+var loginFormEmailInput = document.getElementById("signup_form_email_input");
+var loginFormPasswordTitle = document.getElementById("signup_form_password_title");
+var loginFormPasswordInput = document.getElementById("signup_form_password_input");
+var loginFormSubmitButton = document.getElementById("signup_form_submit_button");
+var loginFormEmailHeader = document.getElementById("signup_form_email_header");
+
+loginForm.style.width = 290 + "px";
+loginForm.style.height = 320 + "px";
+loginForm.style.left = window.innerWidth / 2 - 125 + "px";
+loginForm.style.top = window.innerHeight / 2 - 125 + "px";
+
+loginFormEmailHeader.style.width = 290 + "px";
+loginFormEmailHeader.style.height = 250 + "px";
+loginFormEmailHeader.style.left = 0 + "px";
+loginFormEmailHeader.style.top = 10 + "px";
+
+loginFormEmailTitle.style.width = 250 + "px";
+loginFormEmailTitle.style.height = 250 + "px";
+loginFormEmailTitle.style.left = -5 + "px";
+loginFormEmailTitle.style.top = 30 + "px";
+
+loginFormEmailInput.style.width = 250 + "px";
+loginFormEmailInput.style.height = 25 + "px";
+loginFormEmailInput.style.left = 0 + "px";
+loginFormEmailInput.style.top = 32 + "px";
+
+loginFormPasswordTitle.style.width = 250 + "px";
+loginFormPasswordTitle.style.height = 250 + "px";
+loginFormPasswordTitle.style.left = -5 + "px";
+loginFormPasswordTitle.style.top = 50 + "px";
+
+loginFormPasswordInput.style.width = 250 + "px";
+loginFormPasswordInput.style.height = 25 + "px";
+loginFormPasswordInput.style.left = 0 + "px";
+loginFormPasswordInput.style.top = 52 + "px";
+
+loginFormSubmitButton.style.width = 250 + "px";
+loginFormSubmitButton.style.height = 35 + "px";
+loginFormSubmitButton.style.left = 0 + "px";
+loginFormSubmitButton.style.top = 100 + "px";
