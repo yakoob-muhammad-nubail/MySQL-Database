043f17345fc3bacfb17a1eea6ba104deabe4b39e
yakoobmuhammad <yakooobnmuhammad.4@gmail.com>
2024-08-04 23:46:25 -0400

    log in page frontend with form setup

--- website/static/js/home.js
+++ website/static/js/home.js
@@ -2,6 +2,7 @@ var logoContainer = document.getElementById("logo_container");
 var logoImage = document.getElementById("logo_image");
 var logoText = document.getElementById("logo_text");
 var accountText = document.getElementById("account_text");
+var aboutText = document.getElementById("about_text");
 var loginText = document.getElementById("login_text");
 var signupText = document.getElementById("signup_text");
 var logoutText = document.getElementById("logout_text");
@@ -15,7 +16,12 @@ logoText.style.top = window.innerHeight / 2 - 225 + "px";
 accountText.style.width = 95 + "px";
 accountText.style.height = 25 + "px";
 accountText.style.left = window.innerWidth / 2 - 47.5 + "px";
-accountText.style.top = 160 + "px";
+accountText.style.top = 170 + "px";
+
+aboutText.style.width = 65 + "px";
+aboutText.style.height = 25 + "px";
+aboutText.style.left = window.innerWidth / 2 - 32.5 + "px";
+aboutText.style.top = 160 + "px";
 
 loginText.style.width = 60 + "px";
 loginText.style.height = 25 + "px";
@@ -59,7 +65,18 @@ accountText.addEventListener("mouseover", function hoverOverLogo() {
 
 accountText.addEventListener("mouseout", function hoverOutLogo() {
     // mainNavbarHeaderLogo.src = "../static/images/logo250.png";
+    console.log("hit");
     accountText.style.color = "black";
+});
+
+aboutText.addEventListener("mouseover", function hoverOverLogo() {
+    // mainNavbarHeaderLogo.src = "../static/images/logo250_highlight.png";
+    aboutText.style.color = "blue";
+});
+
+aboutText.addEventListener("mouseout", function hoverOutLogo() {
+    // mainNavbarHeaderLogo.src = "../static/images/logo250.png";
+    aboutText.style.color = "black";
 });
 
 loginText.addEventListener("mouseover", function hoverOverLogo() {
--- website/static/js/log_in.js
+++ website/static/js/log_in.js
@@ -20,19 +20,6 @@ var mainNavbarHeaderNavAccountLogout = document.getElementById("main_navbar_head
 
 var width, height; // throw in preset values and object
 
-var stringEntry = "";
-var stringTitle = "";
-var currentString = "";
-var stringType = "ENTRY";
-var caseState = "LOWER";
-var shiftCase = false;
-var switchCase = false;
-var numberCase = false;
-var counter1 = 0;
-var line1 = 1; // if the line count exceeds two then throw in scroll wheel
-var counter2 = 0;
-var line2 = 1;
-
 const navMenu = {
     navContainerHeight: 90,
     mainNavbarHeaderNavTextWidth: 100,
@@ -40,8 +27,6 @@ const navMenu = {
     clickedMoreItemsState: false,
     clickedAccountState: false
 };
-
-// var menuClick = false;
 
 width = window.innerWidth;
 mainNavbar.style.width = width + "px";
@@ -190,3 +175,49 @@ mainNavbarHeaderNavAccountLogout.addEventListener("mouseout", function hoverOutL
 });
 
 mainNavbarHeaderNavAccountText.addEventListener("mousedown", openCloseAccountMenu);
+
+//form
+var loginForm = document.getElementById("login_form");
+var loginFormEmailTitle = document.getElementById("login_form_email_title");
+var loginFormEmailInput = document.getElementById("login_form_email_input");
+var loginFormPasswordTitle = document.getElementById("login_form_password_title");
+var loginFormPasswordResetTitle = document.getElementById("login_form_password_reset_title");
+var loginFormPasswordInput = document.getElementById("login_form_password_input");
+var loginFormSubmitButton = document.getElementById("login_form_submit_button");
+
+loginForm.style.width = 290 + "px";
+loginForm.style.height = 250 + "px";
+loginForm.style.left = window.innerWidth / 2 - 125 + "px";
+loginForm.style.top = window.innerHeight / 2 - 125 + "px";
+
+loginFormEmailTitle.style.width = 250 + "px";
+loginFormEmailTitle.style.height = 250 + "px";
+loginFormEmailTitle.style.left = -5 + "px";
+loginFormEmailTitle.style.top = 0 + "px";
+
+loginFormEmailInput.style.width = 250 + "px";
+loginFormEmailInput.style.height = 25 + "px";
+loginFormEmailInput.style.left = 0 + "px";
+loginFormEmailInput.style.top = 2 + "px";
+
+loginFormPasswordTitle.style.width = 250 + "px";
+loginFormPasswordTitle.style.height = 250 + "px";
+loginFormPasswordTitle.style.left = -5 + "px";
+loginFormPasswordTitle.style.top = 20 + "px";
+
+loginFormPasswordResetTitle.style.width = 250 + "px";
+loginFormPasswordResetTitle.style.height = 250 + "px";
+loginFormPasswordResetTitle.style.left = 12 + "px";
+loginFormPasswordResetTitle.style.top = 18 + "px";
+
+loginFormPasswordInput.style.width = 250 + "px";
+loginFormPasswordInput.style.height = 25 + "px";
+loginFormPasswordInput.style.left = 0 + "px";
+loginFormPasswordInput.style.top = 22 + "px";
+
+loginFormSubmitButton.style.width = 250 + "px";
+loginFormSubmitButton.style.height = 35 + "px";
+loginFormSubmitButton.style.left = 0 + "px";
+loginFormSubmitButton.style.top = 60 + "px";
+
+
