55cff6cf5c95bb56824116e519b6269c030d79e0
yakoobmuhammad <yakooobnmuhammad.4@gmail.com>
2024-08-05 02:37:42 -0400

    frontend log out page setup

--- website/static/js/log_out.js
+++ website/static/js/log_out.js
@@ -20,18 +20,6 @@ var mainNavbarHeaderNavAccountLogout = document.getElementById("main_navbar_head
 
 var width, height; // throw in preset values and object
 
-var stringEntry = "";
-var stringTitle = "";
-var currentString = "";
-var stringType = "ENTRY";
-var caseState = "LOWER";
-var shiftCase = false;
-var switchCase = false;
-var numberCase = false;
-var counter1 = 0;
-var line1 = 1; // if the line count exceeds two then throw in scroll wheel
-var counter2 = 0;
-var line2 = 1;
 
 const navMenu = {
     navContainerHeight: 90,
@@ -40,8 +28,6 @@ const navMenu = {
     clickedMoreItemsState: false,
     clickedAccountState: false
 };
-
-// var menuClick = false;
 
 width = window.innerWidth;
 mainNavbar.style.width = width + "px";
@@ -189,3 +175,43 @@ mainNavbarHeaderNavAccountLogout.addEventListener("mouseout", function hoverOutL
 });
 
 mainNavbarHeaderNavAccountText.addEventListener("mousedown", openCloseAccountMenu);
+
+//log out window
+var loggedOutWindow = document.getElementById("logged_out_window");
+var loggedOutWindowHeader = document.getElementById("logged_out_window_header");
+var loggedOutWindowLoginOption = document.getElementById("logged_out_window_login_option");
+var loggedOutWindowSignupOption = document.getElementById("logged_out_window_signup_option");
+
+loggedOutWindow.style.width = 295 + "px";
+loggedOutWindow.style.height = 210 + "px";
+loggedOutWindow.style.left = window.innerWidth / 2 - 147.5 + "px";
+loggedOutWindow.style.top = window.innerHeight / 2 - 105 + "px";
+
+loggedOutWindowHeader.style.width = 120 + "px";
+loggedOutWindowHeader.style.height = 25 + "px";
+loggedOutWindowHeader.style.left = 0 + "px";
+loggedOutWindowHeader.style.top = 0 + "px";
+
+loggedOutWindowLoginOption.style.width = 61 + "px";
+loggedOutWindowLoginOption.style.height = 25 + "px";
+loggedOutWindowLoginOption.style.left = 0 + "px";
+loggedOutWindowLoginOption.style.top = 50 + "px";
+
+loggedOutWindowSignupOption.style.width = 75 + "px";
+loggedOutWindowSignupOption.style.height = 25 + "px";
+loggedOutWindowSignupOption.style.left = 180 + "px";
+loggedOutWindowSignupOption.style.top = 7 + "px";
+
+loggedOutWindowLoginOption.addEventListener("mouseover", function hoverOverLogo() {
+    loggedOutWindowLoginOption.style.color = "blue";
+});
+loggedOutWindowLoginOption.addEventListener("mouseout", function hoverOutLogo() {
+    loggedOutWindowLoginOption.style.color = "black";
+});
+
+loggedOutWindowSignupOption.addEventListener("mouseover", function hoverOverLogo() {
+    loggedOutWindowSignupOption.style.color = "blue";
+});
+loggedOutWindowSignupOption.addEventListener("mouseout", function hoverOutLogo() {
+    loggedOutWindowSignupOption.style.color = "black";
+});
